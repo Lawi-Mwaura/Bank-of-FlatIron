700f53765eeca18709024913c0f9c78f8cf09903
Lawi-Mwaura <Lawimwaura@gmail.com>
2023-07-24 11:58:16 +0300

    Polished jsx code and created a good README.md

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Bank-of-FlatIron
+                                 # Bank-of-FlatIron
+# Project Description
+Bank of FlatIron is a web application built with React that allows users to manage their transactions. It provides features for adding new transactions, filtering transactions by description, and displaying the transaction data in a tabular format.
+
+# Project Structure
+The project structure is organized as follows:
+
+BANK-OF-FLATIRON/bank-of-flatiron
+
+  ├── src/
+  │   ├── App.jsx
+  │   ├── TransactionForm.jsx
+  │   ├── SearchBar.jsx
+  │   ├── styles.css
+  │   └── index.css
+  ├── package.json
+  ├── package-lock.json
+  └── README.md
+
+1. src/: This directory contains all the React components and CSS files used in the application.
+
+2. App.jsx: The main component that renders the TransactionForm, SearchBar, and Transaction Table.
+
+3. TransactionForm.jsx: Component for adding new transactions.
+
+4. SearchBar.jsx: Component for filtering transactions based on description.
+
+5. styles.css: CSS file for custom styles.
+
+6. index.css: Default CSS file created by React.
+
+# Dependencies
+The FlatIron project uses the following dependencies:
+
+1. react: JavaScript library for building user interfaces.
+
+2. axios: Promise-based HTTP client for making API requests.
+
+3. react-table: A library for creating tables in React.
+
+# License
+
+Author: Lawi Mwaura
+
+Copyright (c) [2023] [Lawi Mwaura]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
--- bank-of-flatiron/src/App.jsx
+++ bank-of-flatiron/src/App.jsx
@@ -1,29 +1,213 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useTable, useFilters } from 'react-table';
-import logo from './logo.svg';
 import './App.css';
 
+// TransactionForm Component: Form to add new transactions
+const TransactionForm = ({ onAddTransaction }) => {
+  // State to store form data
+  const [formData, setFormData] = useState({
+    date: '',
+    description: '',
+    category: '',
+    amount: '',
+  });
 
-function App() {
+  // Handle form input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTransaction(formData);
+    // Clear form inputs or show a success message
+    setFormData({
+      date: '',
+      description: '',
+      category: '',
+      amount: '',
+    });
+  };
+
   return (
-    <div className="">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      {/* Form fields */}
+      <input
+        type="text"
+        name="date"
+        value={formData.date}
+        onChange={handleChange}
+        placeholder="Date"
+      />
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Description"
+      />
+      <input
+        type="text"
+        name="category"
+        value={formData.category}
+        onChange={handleChange}
+        placeholder="Category"
+      />
+      <input
+        type="number"
+        name="amount"
+        value={formData.amount}
+        onChange={handleChange}
+        placeholder="Amount"
+      />
+      {/* Submit button */}
+      <button type="submit">Add Transaction</button>
+    </form>
+  );
+};
+
+// SearchBar Component: Search input to filter transactions
+const SearchBar = ({ onSearch }) => {
+  // State to store the search term
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Handle search input changes
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Use useEffect to delay the search and prevent excessive calls
+  useEffect(() => {
+    // Create a timer to delay the search
+    const delaySearch = setTimeout(() => {
+      onSearch(searchTerm);
+    }, 300); // Wait for 300 milliseconds after user stops typing before triggering the search
+
+    // Cleanup the timer when component unmounts or the search term changes
+    return () => clearTimeout(delaySearch);
+  }, [searchTerm, onSearch]);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search by description..."
+      value={searchTerm}
+      onChange={handleChange}
+    />
+  );
+};
+
+// BankApp Component: Main component to display transactions and search/filter options
+const BankApp = () => {
+  // State to store all transactions from the API
+  const [transactions, setTransactions] = useState([]);
+
+  // State to store filtered transactions based on search term
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+
+  // Fetch transactions from the API on component mount
+  useEffect(() => {
+    axios.get('http://localhost:8000/transactions')
+      .then((response) => setTransactions(response.data))
+      .catch((error) => {
+        console.error('Error fetching transactions:', error);
+      });
+  }, []);
+
+  // Handle adding a new transaction
+  const handleAddTransaction = (formData) => {
+    axios.post('http://localhost:8000/transactions', formData)
+      .then(() => {
+        // After successful addition, fetch the updated transaction list again from the server
+        axios.get('http://localhost:8000/transactions')
+          .then((response) => setTransactions(response.data))
+          .catch((error) => {
+            console.error('Error fetching transactions:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error adding transaction:', error);
+      });
+  };
+
+  // Handle search to filter transactions
+  const handleSearch = (searchTerm) => {
+    // Filter transactions based on the search term and update the filteredTransactions state
+    const filteredData = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTransactions(filteredData);
+  };
+
+  // Table columns for react-table
+  const columns = [
+    { Header: 'Date', accessor: 'date' },
+    { Header: 'Description', accessor: 'description' },
+    { Header: 'Category', accessor: 'category' },
+    { Header: 'Amount', accessor: 'amount' },
+  ];
+
+  // Use filteredTransactions if search term exists, otherwise use all transactions
+  const data = filteredTransactions.length > 0 ? filteredTransactions : transactions;
+
+  // Create a react-table instance
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+      initialState: {
+        filters: [],
+      },
+    },
+    useFilters
+  );
+
+  // Destructuring necessary properties from the react-table instance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <div>
+      {/* Transaction Form */}
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+
+      {/* Search Bar */}
+      <SearchBar onSearch={handleSearch} />
+
+      {/* Transaction Table */}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default BankApp;
